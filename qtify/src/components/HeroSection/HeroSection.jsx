16f4b3905d3d390c15631d60c84f285395ec020e
rashika <rashika2708@gmail.com>
2024-01-07 19:21:59 +0530

    Describe your changes here

--- qtify/src/components/HeroSection/HeroSection.jsx
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -4,16 +4,13 @@ import styles from './HeroSection.module.css'; // Import the corresponding CSS f
 function  Hero () {
   return (
     <div className={styles.hero}>
-        <div>
-          <h1>100 Thounsand Songs, ad-free </h1>
-          <h1>Over Thounsands podcast episodes </h1>
-
-        </div>
-        <div>
-          <img src={require ("../../assets/hero-headphones.png")} width ={112} alt="headphones"/>
-        </div>
-
-
+      <div>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over Thousands podcast episodes</h1>
+      </div>
+      <div>
+        <img src={require("../../assets/hero-headphones.png")} width={112} height={112} alt="headphones" />
+      </div>
     </div>
   );
 };
