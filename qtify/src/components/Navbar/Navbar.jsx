5d74bc257ca18c3ccfc74b4e23f27a44cccbed56
rashika <rashika2708@gmail.com>
2024-01-11 16:30:25 +0530

    chnages

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Search from '../Search/Search';
 import Button from '../Button/Button';
 import styles from "./Navbar.module.css"
-import { Link } from '@mui/material'
+
 
 
 function Navbar  ({ searchData })  {
   return (
     <nav className={styles.navbar}>
-      <Link to ='/'/>
-      <img src={require ("../../assets/logo.png")} width ={67} alt="Qtify"/>
-      
+      <a href='/'>
+      <div className="logo-container">
+        <img src={require("../../assets/logo.png")} width={67} alt="Qtify" />
+      </div>
+      </a>
       
       <Search placeholder={"Search an album of your choice"}
       searchData={searchData}
--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -4,7 +4,7 @@ import styles from "./Section.module.css"
 import { CircularProgress } from "@mui/material";
 
 export default function Section ({title, data, type}) {
-    const [carouselToggle, setCarouselToggle] = useState(true)
+    const [carouselToggle, setCarouselToggle] = useState(false)
 
     const handleToggle= () =>{
         setCarouselToggle((prevState) => !prevState)
