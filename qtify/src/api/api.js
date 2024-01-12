73efe0e804e706e3aafc13e02f76ff9f70b2a4ba
rashika <rashika2708@gmail.com>
2024-01-12 20:11:44 +0530

    5th

--- qtify/src/api/api.js
+++ qtify/src/api/api.js
@@ -22,7 +22,7 @@ export const fetchNewAlbums = async() => {
 
 export const fetchSongs = async() => {
     try{
-        const response = await axios.get(`${BACKEND_ENDPOINT}/album/songs`)
+        const response = await axios.get(`https://qtify-backend-labs.crio.do/songs`)
         return response.data
     } catch (e){
         console.error(e)
@@ -31,7 +31,7 @@ export const fetchSongs = async() => {
 
 export const fetchFilters = async() => {
     try{
-        const response = await axios.get(`${BACKEND_ENDPOINT}/album/genres`)
+        const response = await axios.get( `https://qtify-backend-labs.crio.do/genres`)
         return response.data
     } catch (e){
         console.error(e)
